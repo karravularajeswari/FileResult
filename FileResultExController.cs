49b4f6e46a4ee6265ec16096ada1e014ce905c92
karravularajeswari <43335512+karravularajeswari@users.noreply.github.com>
2020-04-27 14:46:30 +0530

    Add files via upload

---
+++ FileResultExController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class FileResultExController : Controller
+    {
+        // GET: FileResultEx
+       public FileResult PDFFile()
+        {
+            return File("~/Folder/AdmitCard.pdf","application/pdf");
+        }
+        [HttpGet]
+        public FileResult DocFile()
+        {
+            return File("~/Folder/Resume.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        }
+    }
+}
